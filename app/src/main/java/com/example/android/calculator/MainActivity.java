93cb23dd83aa6e2bae6a39c33642e0f218d2e643
Vatsal <vtslaggarwal@gmail.com>
2017-09-28 20:48:27 +0530

    little changes made

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.android.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
-
+    double input1;
+    double input2;
+    double output;
+    boolean isLong = true;
+    boolean input1entered = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
