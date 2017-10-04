8b95258939addfe8752fe70ea14e7266bce26e3e
Vatsal <vtslaggarwal@gmail.com>
2017-10-04 18:25:42 +0530

    little more functioning

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.android.calculator;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
     double number1;
     double number2;
+    @BindView(R.id.dot)
+    TextView dot;
     boolean input1entered = false;
     char operation;
+    @BindView(R.id.input3)
+    TextView input3;
+    @BindView(R.id.input2)
+    TextView input2;
+    @BindView(R.id.input1)
+    TextView input1;
     TextView[] number = new TextView[10];
     ImageView backspace;
     TextView reset;
@@ -20,19 +31,51 @@ public class MainActivity extends AppCompatActivity {
     TextView minus;
     TextView plus;
     TextView equalTo;
-    TextView input3;
-    TextView input2;
-    TextView input1;
     boolean isOperationEntered;
-    TextView dot;
     TextView sign1;
     TextView sign2;
+    @OnClick(R.id.AC)
+    public void reset(){
+        input1.setText("");
+        input2.setText("");
+        input3.setText("");
+    }
+    @OnClick(R.id.backspace)
+    public void backspace() {
+        if (input1entered) {
+            if (input2.getText().toString().length() == 1)
+                input2.setText("");
+            else if (input2.getText().toString().length() > 1)
+                input2.setText(input2.getText().toString().substring(0, input2.getText().toString().length() - 1));
+        } else if (isOperationEntered) {
+            if (input3.getText().toString().length() == 1)
+                input3.setText("");
+            else if (input3.getText().toString().length() > 1)
+                input3.setText(input3.getText().toString().substring(0, input3.getText().toString().length() - 1));
+        }
+    }
+
+    @OnClick(R.id.dot)
+    public void dot() {
+        if (input1entered) {
+            if (input2.getText().toString().length() == 0)
+                input2.setText("0.");
+            else
+                input2.setText(input2.getText() + ".");
+            setAnswer();
+        } else {
+            if (input3.getText().toString().length() == 0)
+                input3.setText("0.");
+            else
+                input3.setText(input3.getText() + ".");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dot = (TextView) findViewById(R.id.dot);
+        ButterKnife.bind(this);
         sign1 = (TextView) findViewById(R.id.sign1);
         sign2 = (TextView) findViewById(R.id.sign2);
         number[0] = (TextView) findViewById(R.id.number0);
@@ -48,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
         input3 = (TextView) findViewById(R.id.input3);
         input2 = (TextView) findViewById(R.id.input2);
         input1 = (TextView) findViewById(R.id.input1);
-        backspace = (ImageView) findViewById(R.id.backspace);
         for (int i = 0; i < 10; i++) {
             final int I = i;
             number[i].setOnClickListener(new View.OnClickListener() {
@@ -97,57 +139,13 @@ public class MainActivity extends AppCompatActivity {
                 operation = '-';
             }
         });
-        dot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (input1entered) {
-                    if (input2.getText().toString().length() == 0)
-                        input2.setText("0.");
-                    else
-                        input2.setText(input2.getText() + ".");
-                    setAnswer();
-                } else {
-                    if (input3.getText().toString().length() == 0)
-                        input3.setText("0.");
-                    else
-                        input3.setText(input3.getText() + ".");
-                }
-            }
-        });
-        backspace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (input1entered) {
-                    if (input2.getText().toString().length() == 1)
-                        input2.setText("");
-                    else if (input2.getText().toString().length() > 1)
-                        input2.setText(input2.getText().toString().substring(0, input2.getText().toString().length() - 1));
-                } else if (isOperationEntered) {
-                    if (input3.getText().toString().length() == 1)
-                        input3.setText("");
-                    else if (input3.getText().toString().length() > 1)
-                        input3.setText(input3.getText().toString().substring(0, input3.getText().toString().length() - 1));
-                }
-            }
-        });
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                input1.setText("");
-                input2.setText("");
-                input3.setText("");
-
-            }
-        });
-
-
     }
 
     private void setAnswer() {
         try {
             number1 = Double.parseDouble(input1.getText().toString());
             number2 = Double.parseDouble(input2.getText().toString());
-        } catch (NumberFormatException) {
+        } catch (NumberFormatException e) {
             input3.setText("ERR");
             return;
         }
